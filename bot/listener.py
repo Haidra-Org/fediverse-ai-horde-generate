128ecd952cb8ef01966acc27bb6d35aaa725eab6
Divided by Zer0 <mail@dbzer0.com>
2022-11-22 15:43:12 +0100

    aa

--- bot/listener.py
+++ bot/listener.py
@@ -1,5 +1,5 @@
 import requests, json, os, time, argparse, base64, random, re, pprint
-from mastodon.Mastodon import StreamListener
+from mastodon import StreamListener
 from . import args, logger, db_r
 
 
