ad1b0abf899a2a98d31b689788b13f95d3c9bc4c
db0 <mail@dbzer0.com>
2023-06-14 19:50:32 +0200

    fix: forgot f-string:

--- bot/notifications.py
+++ bot/notifications.py
@@ -170,7 +170,7 @@ class MentionHandler:
                         image_body += f"![{media_dict['description']}]({media_dict['url']})"
                     lemmy.post(
                         community_id=community_id,
-                        post_name="{requested_style}: {unformated_prompt}"[0:298],
+                        post_name=f"{requested_style}: {unformated_prompt}"[0:298],
                         post_url=media_dicts[0]["url"],
                         post_body=f"Prompt: {unformated_prompt}\nStyle: {requested_style}\n\n#aiart #stablediffusion{extra_tags}{tags_string}\n\n{image_body}"
                     )
