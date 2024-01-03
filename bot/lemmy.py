8d6451b84a9c3dede1e74cf40c1088b04b105e7f
db0 <mail@dbzer0.com>
2024-01-03 18:30:28 +0100

    increase timeout

--- bot/lemmy.py
+++ bot/lemmy.py
@@ -3,5 +3,5 @@ from pythorhead import Lemmy
 
 lemmy = None
 if os.environ['LEMMY_INSTANCE']:
-    lemmy = Lemmy(f"https://{os.environ['LEMMY_INSTANCE']}")
+    lemmy = Lemmy(f"https://{os.environ['LEMMY_INSTANCE']}", request_timeout=5)
     lemmy.log_in(os.environ['LEMMY_USERNAME'], os.environ['LEMMY_PASSWORD'])
